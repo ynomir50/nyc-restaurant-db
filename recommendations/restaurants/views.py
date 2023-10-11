eb8d3481af7f2840fad0dfc6b0ca621a8fd22e1d
Jason Shu <jason.shu@preludecapital.com>
2023-10-11 10:08:30 -0400

    fixed gitignore

--- recommendations/restaurants/views.py
+++ recommendations/restaurants/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import * 
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "restaurants/index.html")
+
+def about(request):
+    return render(request, "restaurants/about.html")
+
+def list(request):
+    context = {
+        "restaurants":Restaurant.objects.all()
+    }
+    return render(request, "restaurants/list.html", context)
