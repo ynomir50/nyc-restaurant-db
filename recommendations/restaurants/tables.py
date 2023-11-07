15d141dd1dd75988bae5762fbb9009e0ebb8ffe4
Jason Shu <jason.shu@preludecapital.com>
2023-11-07 14:54:33 -0500

    enhanced import, added restaurants, fixed table display for nulls

--- recommendations/restaurants/tables.py
+++ recommendations/restaurants/tables.py
@@ -1,7 +1,7 @@
 # tutorial/tables.py
 import django_tables2 as tables
 from .models import *
-
+import numpy as np
 class RestaurantTable(tables.Table):
     class Meta:
         model = Restaurant
@@ -12,3 +12,8 @@ class RestaurantTable(tables.Table):
         return "$"*value
     def render_rating(self, value):
         return "★"*value
+    def render_comments(self, value): #would like to keep this actually empty instead a string sometime
+        if value == 'nan':
+            return ""     
+        else:
+            return value
--- recommendations/scripts/load_csv.py
+++ recommendations/scripts/load_csv.py
@@ -1,13 +1,18 @@
 from restaurants.models import *
 import pandas as pd
+from django.core.exceptions import ObjectDoesNotExist
 
-def run():
+def run(): #currently deletes all data and then reimports from csv
+    Restaurant.objects.all().delete()
     inDF = pd.read_csv('import/import.csv')
     print(inDF)
-    restaurantimport = [Restaurant(name=obj.Name, neighborhood=Neighborhood.objects.get(name=obj.Neighborhood), cuisine = obj.Cuisine, price = obj.Price, rating = obj.Rating) for obj in inDF.itertuples()]
-    for i in restaurantimport:
-        i.save()
-    # ------can add in filtering to ensure no duplicates later, once I find some criteria to differentiate between entries
-    #for i in restaurantimport:
-    #    if Restaurant.objects.filter(official=i.official).count() == 0: #check if entry exists
-    #        i.save()
+    for obj in inDF.itertuples():
+        try:
+            if Restaurant.objects.filter(name=obj.Name).count() == 0: #check if entry exists before inserting
+                newentry = Restaurant(name=obj.Name, neighborhood=Neighborhood.objects.get(name=obj.Neighborhood), cuisine = obj.Cuisine, price = obj.Price, rating = obj.Rating, comments = obj.Comments)
+                newentry.save()
+            else:
+                print(f"Item not Inserted due to redundancy: {obj.Name}")
+        except ObjectDoesNotExist:
+            print(f"Item not Inserted due to error: {obj.Name}")
+            pass
